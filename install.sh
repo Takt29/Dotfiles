1d4f7caf30089a13fb61b5dfcec6dc759ff5eba6
Yoshida Takuto <takuto@Takutos-MacBook-Pro.local>
2019-06-08 11:28:58 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
 for fileName in .??*
 do
     [[ "$fileName" == ".git" ]] && continue
     [[ "$fileName" == ".DS_Store" ]] && continue
 
     echo "link $fileName to ~/$fileName"
-    ln -s $fileName ~/$fileName
+    ln -s $SCRIPT_DIR/$fileName ~/
+    source ~/$fileName
 done
