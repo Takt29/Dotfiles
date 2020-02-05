b8fa0fd0d02125df8d052d767dfb07f694a00965
Yoshida Takuto <takuto.yoshida@gmail.com>
2020-02-05 23:20:19 +0900

    modify install.sh

--- install.sh
+++ install.sh
@@ -2,25 +2,36 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
+# link dotfiles
+
 for fileName in .??*
 do
-    [[ "$fileName" == ".git" ]] && continue
-    [[ "$fileName" == ".DS_Store" ]] && continue
+    [[ $fileName =~ \~$ ]] && continue
+    [[ $fileName == ".git" ]] && continue
+    [[ $fileName == ".gitignore" ]] && continue
+    [[ $fileName == ".DS_Store" ]] && continue
 
     echo "link $fileName to ~/$fileName"
     ln -s $SCRIPT_DIR/$fileName ~/
 done
 
 set -x
-git config --global core.excludesfile ~/.gitignore_global
 
+# link other files
 mkdir -p ~/Library/KeyBindings
 ln -s $SCRIPT_DIR/DefaultKeyBinding.dict ~/Library/KeyBindings/
+
+# setup git
+
+git config --global core.excludesfile ~/.gitignore_global
 
 # setup brew
 
 if ! [ -x "$(command -v git)" ]; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    echo "install brew"
+    set +x
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    set -x
 fi
 
 brew install rcmdnk/file/brew-file
